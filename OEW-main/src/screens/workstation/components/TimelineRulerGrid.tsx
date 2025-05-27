6e5d389054e4b0d602ca23ff1a9b5247b8da5e89
GitHub Copilot <github-copilot@example.com>
2025-05-27 07:59:39 +0000

    fixes

--- OEW-main/src/screens/workstation/components/TimelineRulerGrid.tsx
+++ OEW-main/src/screens/workstation/components/TimelineRulerGrid.tsx
@@ -1,4 +1,5 @@
-import { useContext, useLayoutEffect, useRef, useState } from "react";
+/** @jsx React.createElement */
+import React, { useContext, useLayoutEffect, useRef, useState } from "react";
 import { PreferencesContext, WorkstationContext } from "../../../contexts";
 import { TimelinePosition, SnapGridSizeOption } from "../../../services/types/types";
 import { formatDuration, measureSeconds } from "../../../services/utils/general";
