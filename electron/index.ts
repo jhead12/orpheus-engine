56a1f4129f44d331bab64a22aba8c4c7a4369d98
Josh Head <jhead12@users.noreply.github.com>
2025-06-07 00:35:02 -0700

    fixes

--- electron/index.ts
+++ electron/index.ts
@@ -1,5 +1,5 @@
 // Export all electron-related functionality
-export { default as MenuBuilder } from './menu';
-export { default as ContextMenuBuilder } from './contextMenu';
-export { setupAudioAnalysisHandlers } from './audioAnalysis';
-export { default as buildHandlers } from './handlers';
+export { default as MenuBuilder } from "./menu";
+export { default as ContextMenuBuilder } from "./contextMenu";
+export { setupAudioAnalysisHandlers } from "./audioAnalysis";
+export { default as buildHandlers } from "./handlers";
