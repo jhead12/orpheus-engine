62fe0c759e7d01068f88b97cba89b441947c4c57
Josh Head <jhead12@users.noreply.github.com>
2025-06-08 18:17:33 -0700

    feat: Set up OEW-main as proper Git submodule tracking feature/server-agnostic-config branch

--- workstation/frontend/OEW-main/src/components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Playhead } from './Playhead';
-export { default as TrackIcon } from './TrackIcon';
-export { default as Automation } from './Automation';
-export { default as StretchAudio } from './StretchAudio';
